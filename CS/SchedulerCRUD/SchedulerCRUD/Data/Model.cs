e5b071157cbd7816234ed217d1caf936f5c641f1
vladimir.zabelin@devexpress.com <vladimir.zabelin@devexpress.com>
2020-01-10 17:56:42 +0300

    fix example content

--- CS/SchedulerCRUD/SchedulerCRUD/Data/Model.cs
+++ CS/SchedulerCRUD/SchedulerCRUD/Data/Model.cs
@@ -10,12 +10,12 @@ namespace SchedulerCRUD.Data
         public int Id { get; set; }
         public int? MedicId { get; set; }
         public string MedicIds { get; set; }
-        public int Status { get; set; } //return nullable after fix
+        public int Status { get; set; }
         public string Subject { get; set; }
         public string Description { get; set; }
-        public int Label { get; set; } //return nullable after fix
-        public DateTime StartTime { get; set; } //return nullable after fix
-        public DateTime EndTime { get; set; } //return nullable after fix
+        public int Label { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public string Location { get; set; }
         public bool AllDay { get; set; }
         public int EventType { get; set; }
