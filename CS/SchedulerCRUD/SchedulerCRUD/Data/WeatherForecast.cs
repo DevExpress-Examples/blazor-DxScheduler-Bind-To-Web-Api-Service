11660c1fe5914a2e97bdd892d1807355a3f95da9
Dan Valeroso <danlestervaleroso@gmail.com>
2022-06-07 15:40:25 +0800

    Updated
    Change to .net 6
    Change version to v22.1.2

--- CS/SchedulerCRUD/SchedulerCRUD/Data/WeatherForecast.cs
+++ CS/SchedulerCRUD/SchedulerCRUD/Data/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace SchedulerCRUD.Data
 {
     public class WeatherForecast
@@ -10,6 +8,6 @@ namespace SchedulerCRUD.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
